9839cd0b2a4a1349c17c98402a4f74243e06262c
Oleg Perminov <personal.perminov@gmail.com>
2017-06-21 17:07:19 +0500

    Improve iptocountry.rb

--- task5/iptocountry.rb
+++ task5/iptocountry.rb
@@ -3,27 +3,21 @@ class MyIp
   require 'ipaddr'
 
   def print_country(input_ip)
-    encoded_ip = encode(input_ip)
-    puts get_country(get_database, encoded_ip)
+    encoded_ip = encode_ip(input_ip)
+    puts find_country(encoded_ip)
   end
 
   private
 
-  def encode(input_string)
-    ipaddr1 = IPAddr.new(input_string)
-    ipaddr1.to_i
+  def encode_ip(input_string)
+    IPAddr.new(input_string).to_i
   end
 
-  def get_database
-    CSV.read('IpToCountry.csv', encoding:'iso-8859-1', skip_lines: /[#]/)
-  end
-
-  def get_country(database_array, number = 0)
+  def find_country(number)
     answer = ''
-    database_array.each do |line|
-      start_range = line[0].to_i
-      end_range = line[1].to_i
-      range = start_range..end_range
+    CSV.foreach('IpToCountry.csv', encoding:'iso-8859-1', \
+      converters: :numeric, skip_lines: /[#]/) do |line|
+      range = line[0]..line[1]
       if range.include?(number)
         answer = line[4]
         break
@@ -33,5 +27,5 @@ class MyIp
   end
 end
 
-ip_adress = !ARGV[0].nil? ? ARGV[0] : '0'
+ip_adress = !ARGV[0].nil? ? ARGV[0] : '0.0.0.0'
 MyIp.new.print_country(ip_adress)
