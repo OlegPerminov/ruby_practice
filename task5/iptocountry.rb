7ce8e241ef9f795e7ce802d33a44571e14ab3e1f
Oleg Perminov <personal.perminov@gmail.com>
2017-06-23 17:25:58 +0500

    Add range tree realization

--- task5/iptocountry.rb
+++ task5/iptocountry.rb
@@ -1,10 +1,20 @@
 class MyIp
   require 'csv'
   require 'ipaddr'
+  require 'segment_tree'
+
+  def initialize
+    database = {}
+    CSV.foreach('IpToCountry.csv', encoding:'iso-8859-1', \
+      converters: :numeric, skip_lines: /[#]/) do |row|
+      database[row[0]..row[1]] = row[4]
+    end
+    @ip_tree = SegmentTree.new(database)
+  end
 
   def print_country(input_ip)
     encoded_ip = encode_ip(input_ip)
-    puts find_country(encoded_ip)
+    puts @ip_tree.find(encoded_ip).value
   end
 
   private
@@ -12,20 +22,8 @@ class MyIp
   def encode_ip(input_string)
     IPAddr.new(input_string).to_i
   end
-
-  def find_country(number)
-    answer = ''
-    CSV.foreach('IpToCountry.csv', encoding:'iso-8859-1', \
-      converters: :numeric, skip_lines: /[#]/) do |line|
-      range = line[0]..line[1]
-      if range.include?(number)
-        answer = line[4]
-        break
-      end
-    end
-    answer
-  end
 end
 
-ip_adress = !ARGV[0].nil? ? ARGV[0] : '0.0.0.0'
-MyIp.new.print_country(ip_adress)
+ip_finder = MyIp.new
+ip_adress = ARGV[0].nil? ? '0.0.0.0' : ARGV[0]
+ip_finder.print_country(ip_adress)
