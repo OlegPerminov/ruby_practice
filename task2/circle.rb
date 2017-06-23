206e229e11856e0938c9a9f55561aaaf9d5c0708
Oleg Perminov <personal.perminov@gmail.com>
2017-06-23 15:58:21 +0500

    Improve task2 file circle.rb

--- task2/circle.rb
+++ task2/circle.rb
@@ -1,39 +1,45 @@
+class Point
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+end
+
 class Circle
-  attr_accessor :coord_x, :coord_y, :radius
+  attr_reader :center, :radius
 
   def initialize(x, y, r)
-    @coord_x = x
-    @coord_y = y
+    @center = Point.new(x, y)
     @radius = r
   end
 
-  def print_incoming_points(input_array)
-    print check(input_array).to_s + "\n"
+  def check_points(input_array)
+    input_points = create_points(input_array)
+    included_points = input_points.select { |point| include?(point) }
+    get_coordinates(included_points)
   end
 
   private
 
-  def check(input_points)
-    areas_points = []
-    input_points.each do |coord|
-      if in_circle?(coord)
-         areas_points << coord
-      end
-    end
-    areas_points
+  def create_points(numbers_array)
+    numbers_array.map { |item| Point.new(item[0], item[1]) }
   end
 
-  def in_circle?(coord)
-    (coord[0] - @coord_x)**2 + (coord[1] - @coord_y)**2 <= @radius**2
+  def get_coordinates(points_array)
+    points_array.map { |point| [point.x, point.y] }
+  end
+
+  def include?(point)
+    (@center.x - point.x)**2 + (@center.y - point.y)**2 <= @radius**2
   end
 end
 
 test_points = [[0, 0], [1, 0], [0, 1], [1, 1], [2, 2], [0.5, 0.5]]
-
 puts "Initial array is: \n#{test_points}"
 
 my_circle = Circle.new(0, 0, 1)
-
-print "The circle with center in x = #{my_circle.coord_x}, \
-y = #{my_circle.coord_y} and radius = #{my_circle.radius} have: \n"
-my_circle.print_incoming_points(test_points)
+puts "The circle with center in x = #{my_circle.center.x}, \
+y = #{my_circle.center.y} and radius = #{my_circle.radius} have:"
+puts my_circle.check_points(test_points).to_s
